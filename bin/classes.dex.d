c42cbd7d24efa381045778c4a0290fd08689813e
duyonghong <duyonghongncut@163.com>
2016-02-29 12:52:11 +0800

    1

--- bin/classes.dex.d
+++
@@ -1,9 +0,0 @@
-E:\hh\workspace\autotesting\bin\classes.dex : \
-E:\hh\workspace\autotesting\bin\classes\com\ableclassroom\test\aaa.class \
-E:\hh\workspace\autotesting\bin\classes\com\ableclassroom\test\login.class \
-E:\hh\workspace\autotesting\bin\classes\com\ableclassroom\test\UiAutomatorHelper.class \
-E:\hh\workspace\autotesting\bin\classes\com\app\test\testing.class \
-E:\hh\workspace\autotesting\bin\classes\com\app\test\UiAutomatorHelper.class \
-E:\hh\workspace\autotesting\bin\classes\com\onlineschool\test\login.class \
-E:\hh\workspace\autotesting\bin\classes\com\onlineschool\test\testing.class \
-E:\hh\workspace\autotesting\bin\classes\com\onlineschool\test\UiAutomatorHelper.class \
