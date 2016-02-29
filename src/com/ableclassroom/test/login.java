1c043c43e7c072a8f5cfdd321768359ec3f86e81
duyonghong <duyonghongncut@163.com>
2016-02-29 12:56:12 +0800

    1

--- src/com/ableclassroom/test/login.java
+++ src/com/ableclassroom/test/login.java
@@ -4,7 +4,7 @@ import com.android.uiautomator.testrunner.UiAutomatorTestCase;
 
 public class login extends UiAutomatorTestCase {
 	public void testlogin(){
-		System.out.println("111111111122");
+		System.out.println("111111111111");
 	}
 
 }
