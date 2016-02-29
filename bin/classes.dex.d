93118bd8c4ab5011301b8cbc788bba3e2f470f94
duyonghong <duyonghongncut@163.com>
2016-02-29 13:31:12 +0800

    2

---
+++ bin/classes.dex.d
@@ -0,0 +1,5 @@
+C:\Users\Workspace\autotesting\bin\classes.dex : \
+C:\Users\Workspace\autotesting\bin\classes\com\ableclassroom\test\login.class \
+C:\Users\Workspace\autotesting\bin\classes\com\app\test\testing.class \
+C:\Users\Workspace\autotesting\bin\classes\com\app\test\UiAutomatorHelper.class \
+C:\Users\Workspace\autotesting\bin\classes\com\onlineschool\test\login.class \
--- src/com/app/test/testing.java
+++ src/com/app/test/testing.java
@@ -8,9 +8,9 @@ public class testing extends UiAutomatorTestCase {
 	public static void main(String[] args){
 		String jarName = "test";
 		// ²âÊÔÍøĞ£¿ÎÌÃ
-		String testClass = "com.onlineschool.test.login";
+//		String testClass = "com.onlineschool.test.login";
 		// ²âÊÔÄÜÁ¦¿ÎÌÃ
-//		String testClass = "com.ableclassroom.test.login";
+		String testClass = "com.ableclassroom.test.login";
 		String testName = "";
 		String androidId = "10";
 		new UiAutomatorHelper(jarName, testClass, testName, androidId);
