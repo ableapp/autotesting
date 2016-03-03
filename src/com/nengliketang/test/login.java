12d07a0bd379e6a7d3fdbc285d961426042c05c0
duyonghong <duyonghongncut@163.com>
2016-03-03 21:48:55 +0800

    登录

--- src/com/nengliketang/test/login.java
+++ src/com/nengliketang/test/login.java
@@ -8,7 +8,12 @@ public class login extends UiAutomatorTestCase {
 	public void testlogin() {
 		Publicclass.wakeUp();
 		Publicclass.slideUnlock();
-		System.out.println("1111111111");
+		new Publicclass().findablesky();
+		Publicclass.clickMy();
+		Publicclass.clickLoginAndRegistButton();
+		new Publicclass().inputUser("zidonghuaceshi");
+		new Publicclass().inputPassport("aaaa");
+		new Publicclass().clickLoginButton();
 	}
 
 }
