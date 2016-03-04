37197fd8cdf25c89049f1acb687a7d8a5c0e6b21
duyonghong <duyonghongncut@163.com>
2016-03-04 10:15:04 +0800

    自动产生的bin文件

--- bin/classes.dex.d
+++
@@ -1,5 +0,0 @@
-E:\hh\workspace\nengliketang\bin\classes.dex : \
-E:\hh\workspace\nengliketang\bin\classes\com\app\test\Publicclass.class \
-E:\hh\workspace\nengliketang\bin\classes\com\app\test\testing.class \
-E:\hh\workspace\nengliketang\bin\classes\com\app\test\UiAutomatorHelper.class \
-E:\hh\workspace\nengliketang\bin\classes\com\nengliketang\test\login.class \
