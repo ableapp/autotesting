ec0cd40acd5544b915ff2b63ba178c1703de6e68
duyonghong <duyonghongncut@163.com>
2016-03-15 17:56:41 +0800

    多余文件

---
+++ bin/classes.dex.d
@@ -0,0 +1,5 @@
+C:\Users\Workspace\nengliketang\bin\classes.dex : \
+C:\Users\Workspace\nengliketang\bin\classes\com\app\test\Publicclass.class \
+C:\Users\Workspace\nengliketang\bin\classes\com\app\test\testing.class \
+C:\Users\Workspace\nengliketang\bin\classes\com\app\test\UiAutomatorHelper.class \
+C:\Users\Workspace\nengliketang\bin\classes\com\nengliketang\test\login.class \
